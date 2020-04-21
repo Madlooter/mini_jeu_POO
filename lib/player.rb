7be4a0c0416b326e284df8f460acc63f8575d66e
Madlooter <morelle.antoine@gmail.com>
2020-04-21 16:21:05 +0200

    Nouveau push de la V2 du minijeu

--- lib/player.rb
+++ lib/player.rb
@@ -1,41 +1,92 @@
-
-
 class Player
-	attr_accessor :name, :life_points
+  attr_accessor :name, :life_points
+  @@players_count = 0
+  @@all_players = []
 
-	def initialize(name, life_points=10)
-		@name = name
-		@life_points = life_points
-	end
+  ## CREATE NEW PLAYER
+  def initialize(name)
+    @name = name
+    @life_points = 10
+    @@all_players << self
+  end
 
-	def self name
-		return name
-	end
+  def show_state
+    "#{@name} a #{@life_points} PV."
+  end
 
-	def self life_points
-		return life_points
-	end
+  def gets_damage(damages)
+    @life_points -= damages
+    if @life_points <= 0
+      "† #{name} a été tué ! †"
+    end
+  end
 
-	def show_state
-		"#{name} a #{life_points} points de vie."
-	end
+  def attacks(player)
+    puts "## FIGHT ## #{name} attaque #{player.name} !"
+    damages = compute_damage
+    player.gets_damage(damages)
+    puts "Il lui inflige #{damages} dommages."
+    if player.life_points <= 0
+      puts "† #{player.name} est mort sur le coup ! †"
+    end
+  end
 
-	def gets_damage(damage)
-		@life_points -= damage
-		if life_points <= 0
-			puts "le joueur #{name} a été tué !"
-		end
-	end
+  def compute_damage
+    return rand(1..6)
+  end
 
-	def attacks(attacked_player)
-		damage_inflicted = compute_damage
-		puts "Le joueur #{name} attaque le joueur #{attacked_player.name}"
-		puts "Il lui inflige #{damage_inflicted} points de dommages"
-		attacked_player.gets_damage(damage_inflicted)
-	end
+end
 
-	def compute_damage
-    	return rand(1..6)
-  	end
+class HumanPlayer < Player
+  attr_accessor :weapon_level
+  
+  def initialize(name)
+    @weapon_level = 1
+    @name = name
+    @life_points = 100
+    @@all_players << self
+  end
 
-end 
+  def show_state
+    "Tu as #{@life_points} PV et une arme de niveau #{weapon_level}."
+  end
+
+  def compute_damage
+    return rand(1..6) * weapon_level
+  end
+
+  def search_weapon
+    dice = rand(1..6)
+    puts "#{name} a trouvé une arme de niveau #{dice}"
+    if dice > @weapon_level
+      @weapon_level = dice
+      puts "Youhou ! elle est meilleure que ton arme actuelle : tu la prends !"
+    else 
+      "M@*#$... elle n'est pas mieux que ton arme actuelle..."
+    end
+  end
+
+  def search_health_pack
+    dice = rand(1..6)
+    if dice == 1
+      puts "... Tu n'as rien trouvé !"
+    elsif dice > 1 && dice < 6
+      puts "Tu as trouvé un pack de 50 PV !"
+      @life_points += 50
+      if @life_points > 100
+        @life_points = 100
+      end
+      puts "Tu as maintenant #{life_points} PV !"
+    else 
+      puts "WOOOOW ! C'est sun pack de 80 PV!"
+      @life_points += 80
+      if @life_points > 100
+        @life_points = 100
+      end
+      puts "Tu as maintenant #{life_points} PV !"
+    end
+  end
+
+
+
+end
